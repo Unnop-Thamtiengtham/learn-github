d1c912eadf51dc3ee70a22dd5efbad1a3e971747
Unnop-Thamtiengtham <122899012+Unnop-Thamtiengtham@users.noreply.github.com>
2026-02-11 23:19:44 +0700

    Update app.js
    101

--- Pattice_Unnop/Pattice_oop/app.js
+++ Pattice_Unnop/Pattice_oop/app.js
@@ -3,6 +3,7 @@ class User {
     // private
     #name
     #password
+    static type = "ผู้ใช้งานระบบ"
     // parameter constuctor
     constructor (name, password){
         this.#name = name
@@ -12,8 +13,25 @@ class User {
     showDetail(){
         console.log(`Username ${this.#name} Password ${this.#password}`)
     }
+
+    // setter method
+    set Name (newName){
+        this.#name = newName
+    }
+
+    // getter method
+    get Name (){
+        return this.#name
+    }
+
+    static showType (){
+        console.log("I'm a User ")
+    }
 }
 
 // create object
 const user1 = new User("beem",123);
-user1.showDetail()
+user1.Name = "Unnop"
+User.showType()
+console.log(User.type)
+console.log(user1.Name)
